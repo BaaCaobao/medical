c727fca1805c2c8faacc8794b4956fe69077a630
草包 <203557550@qq.com>
2022-08-19 10:08:24 +0800

    c

--- src/main/java/com/medical/controller/MunityController.java
+++ src/main/java/com/medical/controller/MunityController.java
@@ -2,14 +2,15 @@ package com.medical.controller;
 
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.medical.entity.Munity;
+import com.medical.mapper.MunityMapper;
 import com.medical.service.MunityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -25,13 +26,37 @@ public class MunityController {
     @Autowired
     private MunityService munityService;
 
-    @PutMapping("updateMunityCtate")
-    public void updateMunity(@RequestBody  Munity munity){
-        UpdateWrapper<Munity> munityUpdateWrapper=new UpdateWrapper<>();
-        munityUpdateWrapper.eq("id",munity.getId()).set("c_state","封号");
-        munityService.update(munityUpdateWrapper);
+
+    @Autowired
+    private MunityMapper munityMapper;
+
+
+    /**
+     * 修改文章状态
+     * @param munity 社区文章类
+     * @return 是否修改成功
+     */
+    @PutMapping("/updateMunityCtate")
+    public boolean updateMunity(@RequestBody Munity munity) {
+        UpdateWrapper<Munity> munityUpdateWrapper = new UpdateWrapper<>();
+        munityUpdateWrapper.eq("id", munity.getId()).set("c_state", "下架");
+        boolean update = munityService.update(munityUpdateWrapper);
+        return update;
     }
 
+    /**
+     * 查询文章数量
+     * @return 返回文章数量
+     */
 
+    @GetMapping("/selectAllnumber")
+    public int selectAllnumber() {
+        QueryWrapper<Munity> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("COUNT(*) as id");
+        List<Munity> list = munityService.list(queryWrapper);
+        int id = list.get(0).getId();
+        System.out.println(id);
+        return id;
+    }
 
 }
